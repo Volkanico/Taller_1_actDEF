23ac121f4a6bb9f6313403b18d539b350168991b
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2020-11-16 09:07:07 +0100

    S'ha afegit un nou espai d'obra (obra2) al Main

--- src/com/company/Autor.java
+++ src/com/company/Autor.java
@@ -2,17 +2,29 @@ package com.company;
 
 public class Autor {
     private String name;
-    String[] llistaObres;
+    private Obra [] llistaObres;
 
 
-    public Autor() {
-        public Autor (String name, String llistaObres);
+    public Autor(String name, Obra [] llistaObres) {
         this.name = name;
         this.llistaObres = llistaObres;
     }
     public Obra obraMesLlarga() {
+        this.llistaObres[0].getNumPagines();
+        int maxPagines = 0;
+        Obra obra = new Obra();
 
+        for(int i=0; i < this.llistaObres.length; i++) {
+            if(this.llistaObres[i].getNumPagines() > maxPagines) {
+                maxPagines = this.llistaObres[i].getNumPagines();
+                obra = this.llistaObres[i];
+            }
+        }
+        return obra;
     }
+
+
+
 
 }
 
